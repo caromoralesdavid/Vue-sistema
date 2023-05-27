ff95b2bce0f479c3c565b1d36b6ca7c125a7eb95
David Caro Morales <ing.caromoralesdavid@gmail.com>
2023-05-27 17:57:03 -0500

    estilos al login, personalización css

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,23 +1,89 @@
 <template>
-  <div>
+  <div class="login">
     <h2>Iniciar sesión</h2>
-    <form @submit.prevent="login">
-      <div>
+    <form @submit.prevent="login" class="form">
+      <div class="user">
         <label for="username">Usuario:</label>
-        <input type="text" id="username" v-model="username" required />
+        <input type="text" id="username" v-model="username" placeholder="Ingrese su usuario..." required />
       </div>
-      <div>
+      <div class="password">
         <label for="password">Contraseña:</label>
-        <input type="password" id="password" v-model="password" required />
+        <input type="password" id="password" v-model="password" placeholder="Ingrese su contraseña..." required />
       </div>
       <button type="submit">Iniciar sesión</button>
-    </form>
+    </form> <br>
     <div>
       <a href="/reset-password">¿Olvidaste tu contraseña?</a>
     </div>
   </div>
 </template>
 
+<style>
+  .login {
+    width: 300px;
+    margin: 0 auto;
+    margin-top: 100px;
+    background-color: #f2f2f2;
+    border-radius: 5px;
+    padding: 20px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+
+  h2 {
+    text-align: center;
+    color: #007bff;
+    font-weight: bold;
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .user,
+  .password {
+    margin-bottom: 10px;
+  }
+
+  label {
+    color: #007bff;
+    font-weight: bold;
+    font-size: 14px;
+  }
+
+  input[type="text"],
+  input[type="password"] {
+    width: 100%;
+    padding: 8px;
+    border-radius: 3px;
+    border: 1px solid #007bff;
+    font-size: 14px;
+  }
+
+  button[type="submit"] {
+    background-color: #007bff;
+    color: #ffffff;
+    border: none;
+    padding: 10px 16px;
+    margin-top: 20px;
+    border-radius: 3px;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  button[type="submit"]:hover {
+    background-color: #00bcd4;
+  }
+
+  a {
+    color: #007bff;
+    font-size: 14px;
+    text-decoration: none;
+  }
+</style>
 <script>
 export default {
   data() {
