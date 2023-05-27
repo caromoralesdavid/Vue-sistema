b133a50199fddce96ee89d78593dc8f900afc090
David Caro Morales <ing.caromoralesdavid@gmail.com>
2023-05-27 15:33:10 -0500

    estilos aplicados al app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
-  <div>
+  <div class="encabezado">
     <header>
       <h1>CONTROL DE ASISTENCIAS</h1>
+
     </header>
 
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/components/icons/LogoAsistencia.png"
-      width="125"
-      height="125"
-    />
+    <img alt="Vue logo" class="logo" src="@/components/icons/LogoAsistencia.png" width="125" height="125" />
     <div class="content">
-      <!-- <div class="wrapper"> -->
-      <!-- <HelloWorld msg="CC ASISTENCIAS" /> -->
-      <label for="input-text">Ingrese su cédula:</label>
+      <label for="input-text">Ingrese su cédula:</label><br>
       <!-- <input type="text" id="input-text" v-model="cedula" /> -->
-      <input type="text" id="input-text" v-model="cedula" autocomplete="off" />
-
+      <input type="text" id="input-text" v-model="cedula" autocomplete="off" /><br><br>
       <button class="entrada" @click="marcarEntrada">Entrada</button>
-      <button class="entrada" @click="marcarSalida">Salida</button>
+      <button class="salida" @click="marcarSalida">Salida</button>
     </div>
-
+    <br>
     <router-view></router-view>
-    <!-- Utiliza "router-view" en lugar de "RouterView" -->
-    <router-link to="/dashboard">Acceso</router-link>
-    <!-- Utiliza "router-link" en lugar de "RouterLink" -->
+    <div class="acceso">
+      <router-link to="/login" target="_blank">Acceso</router-link>
+    </div>
   </div>
 </template>
 
@@ -48,3 +40,65 @@ export default {
   },
 };
 </script>
+
+<style>
+body {
+  padding: 10px;
+  text-align: center;
+  align-items: center;
+  background-color: rgb(158, 202, 187);
+}
+
+.content {
+  font-size: 14px;
+  font-weight: bold;
+  font-family: 'Times New Roman', Times, serif;
+}
+
+.content input {
+  border-radius: 10px;
+}
+
+.encabezado {
+  font-size: 14px;
+  font-weight: bold;
+}
+
+img {
+  border-radius: 50%;
+}
+
+.entrada {
+  background-color: greenyellow;
+  size: 18px;
+  font-size: 14px;
+  border-radius: 10px;
+}
+
+.entrada:hover {
+  background-color: rgb(49, 250, 49);
+  cursor: pointer;
+  color: antiquewhite;
+}
+
+.salida {
+  background-color: rgb(245, 0, 0);
+  size: 18px;
+  font-size: 14px;
+  border-radius: 10px;
+}
+
+.salida:hover {
+  background-color: red;
+  cursor: pointer;
+  color: antiquewhite;
+}
+.acceso{
+  font-weight: bold;
+  font-size: 14px;
+}
+.acceso:hover{
+  cursor: pointer;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/CrearUsuario.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h2>Crear nuevo usuario</h2>
+      <form>
+        <label for="username">Nombre de usuario:</label>
+        <input type="text" id="username" required />
+  
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" required />
+  
+        <button type="submit">Crear usuario</button>
+      </form>
+    </div>
+  </template>
